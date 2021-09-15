60ac49c40c6f31f4312099b27e32834df5ce0eed
BuildTools <unconfigured@null.spigotmc.org>
2021-09-15 13:38:03 -0500

    added zgauntlet

--- zgauntlet/zgauntlet.gradle.kts
+++ zgauntlet/zgauntlet.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Owain van Brakel <https:github.com/Owain94>
+ * Copyright (c) 2019 Owain van Brakel <https://github.com/Owain94>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -23,18 +23,22 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-rootProject.name = "BLPlugins"
-include(":entityhiderextension")
-include(":nightmare")
-include(":ztob")
-include(":zgauntlet")
 
-for (project in rootProject.children) {
-    project.apply {
-        projectDir = file(name)
-        buildFileName = "$name.gradle.kts"
+version = "1.0.1"
 
-        require(projectDir.isDirectory) { "Project '${project.path} must have a $projectDir directory" }
-        require(buildFile.isFile) { "Project '${project.path} must have a $buildFile build script" }
+project.extra["PluginName"] = "BLGauntlet"
+project.extra["PluginDescription"] = "Bluelite Gauntlet plugin"
+
+tasks {
+    jar {
+        manifest {
+            attributes(mapOf(
+                    "Plugin-Version" to project.version,
+                    "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
+                    "Plugin-Provider" to project.extra["PluginProvider"],
+                    "Plugin-Description" to project.extra["PluginDescription"],
+                    "Plugin-License" to project.extra["PluginLicense"]
+            ))
+        }
     }
 }
